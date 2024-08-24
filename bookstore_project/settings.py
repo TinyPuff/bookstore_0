d32a6ac970d51d90a92f12c27c632daa0d9e2fb2
TinyPuff <ghost1of1king@gmail.com>
2024-08-25 00:33:51 +0330

    ready for deployment

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -252,6 +252,6 @@ if ENVIRONMENT=="production":
     SECURE_CONTENT_TYPE_NOSNIFF = True
 
 # Heroku
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
