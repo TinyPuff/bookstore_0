a60eaf0b4bcdcac3218e21e04939a98ffa2be30d
TinyPuff <ghost1of1king@gmail.com>
2024-09-11 00:57:35 +0330

    removed unnecessary imports

--- users/models.py
+++ users/models.py
@@ -1,8 +1,4 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator
-from books.models import Book
-from allauth.account.models import EmailAddress
 
 # Create your models here.
 
