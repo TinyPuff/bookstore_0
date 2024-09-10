c4c038c69fbe9e9db4d5901efde0a79f3ad2ad46
TinyPuff <ghost1of1king@gmail.com>
2024-09-11 01:28:28 +0330

    added ProfileForm

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import Profile
+from django import forms
 
 
 
@@ -17,3 +19,10 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = ('email', 'username', )
+
+
+class ProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ('email', 'age', 'address', 'zipcode',)
