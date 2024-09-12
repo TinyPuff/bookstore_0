b1877885eb8b8fb6a9eeed7baf546f9fd5358ff2
TinyPuff <ghost1of1king@gmail.com>
2024-09-12 16:33:18 +0330

    removed unnecessary import

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from books.models import Book
 from django.core.exceptions import ValidationError
 from allauth.account.models import EmailAddress
@@ -7,24 +6,28 @@ from django.urls import reverse
 
 # Create your models here.
 
+
 class OrderInfo(models.Model):
     gateway_id = models.PositiveIntegerField()
     user = models.ForeignKey(EmailAddress, on_delete=models.CASCADE)
     price = models.PositiveIntegerField(default=0)
     tracking_code = models.PositiveBigIntegerField()
-    post_tracking_code = models.PositiveBigIntegerField(default=0) # this one is different from the tracking code seen in the bank gateway. it's for the postal office.
+    post_tracking_code = models.PositiveBigIntegerField(
+        default=0
+    )  # this one is different from the tracking code seen in the bank gateway. it's for the postal office.
     created_at = models.DateTimeField(blank=True, null=True)
     status = models.CharField(max_length=200, default="")
     total_products = models.PositiveIntegerField(default=0)
 
     class Meta:
-        verbose_name_plural = 'Order Info'
+        verbose_name_plural = "Order Info"
 
     def __str__(self):
         return f"{self.user} ({self.gateway_id})"
-    
+
     def get_absolute_url(self):
-        return reverse('order', args=[str(self.id)])
+        return reverse("order", args=[str(self.id)])
+
 
 class OrderedProductsInfo(models.Model):
     order = models.ForeignKey(OrderInfo, on_delete=models.CASCADE)
@@ -34,7 +37,9 @@ class OrderedProductsInfo(models.Model):
     def __str__(self):
         return f"{self.quantity} x {self.product}"
 
+
 # add purchased products page
+
 
 class Cart(models.Model):
     user = models.ForeignKey(EmailAddress, on_delete=models.CASCADE)
@@ -48,9 +53,9 @@ class Cart(models.Model):
             elif self.product.stock > 1:
                 msg = f"There are only {self.product.stock} in stock."
             elif self.product.stock == 0:
-                msg= "There is none in stock."
-            raise ValidationError({'quantity': msg})
-    
+                msg = "There is none in stock."
+            raise ValidationError({"quantity": msg})
+
     # use the session framework to store the cart items
 
     def __str__(self):
